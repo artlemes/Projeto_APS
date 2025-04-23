28b49a3f9333df189b074d8b4c85769d8c0d0d0a
enzogsuhnel <enzogsuhnel@gmail,com>
2025-04-23 03:02:54 -0300

    Login funcionando (buscando, auntenticando e salvando sessão), tela inicial criada, logout funcional, tela profile abrindo. ** Efetuada alterações na maneira com que a classes controllers conversam (atualmente mais proximo do que deve ser um MVC).

---
+++ Projeto APS - EnzoMadruga/venv/controller/main_controller.py
@@ -0,0 +1,39 @@
+from view.main_view import MainView
+
+from controller.users_controller import UserController
+
+class MainController:
+    def __init__(self, root):
+        self.root = root
+        self.current_view = None
+        self.__user_controller = UserController(self.root, self)
+        self.__main_view = MainView(self.root, self)
+        self.show_main()
+
+    def clear_window(self):
+        for widget in self.root.winfo_children():
+            widget.destroy()
+
+    def show_main(self):
+        self.clear_window()
+        self.__main_view.first_screen()
+
+    def main_login(self):
+        self.clear_window()
+        self.__user_controller.show_login_screen()
+    
+    def main_profile(self):
+        self.clear_window()
+        self.__user_controller.show_profile_screen()
+
+    def main_logout(self):
+        self.clear_window()
+        self.__user_controller.show_logout_screen()
+
+    def main_cadastro(self):
+        self.clear_window()
+        self.__user_controller.show_register_screen()
+    
+    def show_home(self):
+        self.clear_window()
+        self.__main_view.home_screen()
---
+++ Projeto APS - EnzoMadruga/venv/db/connection.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+# Carrega as variáveis do arquivo .env
+load_dotenv()
+
+def get_database():
+    CONNECTION_STRING = os.getenv("MONGODB_URI")
+    
+    if not CONNECTION_STRING:
+        raise ValueError("CONNECTION_STRING não encontrada no .env")
+
+    client = MongoClient(CONNECTION_STRING)
+    return client["FitTrack"]
