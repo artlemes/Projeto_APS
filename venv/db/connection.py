7142270a1888818a51a85d5873ac123031df1845
Enzo Guidi Suhnel <121643240+enzogsuhnel@users.noreply.github.com>
2025-04-23 03:05:33 -0300

    Delete venv directory

--- venv/db/connection.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-
-def get_database():
-    CONNECTION_STRING = "mongodb+srv://fittrack:trabalhodeaps2025@fittrack.gkwa6ir.mongodb.net/?retryWrites=true&w=majority&appName=FitTrack"
-    
-    client = MongoClient(CONNECTION_STRING)
-    return client["FitTrack"]  # esse é o nome do banco de dados
