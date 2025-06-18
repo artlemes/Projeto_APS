5d3758c9b3d5d3784806379fec5521dd6a99221d
enzogsuhnel <enzogsuhnel@gmail,com>
2025-06-18 16:41:24 -0300

    alt: sync connection

--- venv/db/connection.py
+++ venv/db/connection.py
@@ -13,3 +13,11 @@ def get_database():
 
     client = MongoClient(CONNECTION_STRING)
     return client["FitTrack"]
+
+def users_collection():
+    db = get_database()
+    return db["users"]
+
+def workout_plans_collection():
+    db = get_database()
+    return db["workout_plans"]
